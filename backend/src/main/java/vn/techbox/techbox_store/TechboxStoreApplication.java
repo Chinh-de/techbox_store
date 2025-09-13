f6acce27f129076d69bcedf24f352194d3fdd2ef
ChinhDe <chinhde29@gmail.com>
2025-09-13 08:10:50 +0700

    init docker

---
+++ backend/src/main/java/vn/techbox/techbox_store/TechboxStoreApplication.java
@@ -0,0 +1,13 @@
+package vn.techbox.techbox_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechboxStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechboxStoreApplication.class, args);
+	}
+
+}
